20ee9cd8fc9fb305169ec9eacfd470486396e117
mertCukadar <cukadar.mertkaan@gmail.com>
2023-08-10 13:57:44 +0300

    fix bugs

--- Source/main.cpp
+++ Source/main.cpp
@@ -2,15 +2,6 @@
 
 int main() {
     if (wsInit()) {
-        SOCKET sock;
-        if (CreateSocket(sock, SOCK_STREAM, Port)) {
-            std::cout << "Socket created and specified port closed." << std::endl;
-        } else {
-            std::cerr << "Error creating socket or binding." << std::endl;
-        }
-        CloseSocket(sock);
-
-        // Run a program using the system function
         const char *programToRun = "your_program.exe"; // Change this to the program you want to run
         int result = system(programToRun);
         if (result == 0) {
@@ -21,6 +12,18 @@ int main() {
     } else {
         std::cerr << "WSA initialization failed." << std::endl;
     }
+
+    
+        SOCKET sock;
+        if (CreateSocket(sock, SOCK_STREAM, Port)) {
+            std::cout << "Socket created and specified port closed." << std::endl;
+        } else {
+            std::cerr << "Error creating socket or binding." << std::endl;
+        }
+        CloseSocket(sock);
+
+        // Run a program using the system function
+        
 
     return 0;
 }
