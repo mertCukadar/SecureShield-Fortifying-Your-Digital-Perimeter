c5c0c69fca1652bc4bfcaca49806985f750caf79
mertCukadar <cukadar.mertkaan@gmail.com>
2023-08-10 13:54:02 +0300

    run program after close specified socket

---
+++ Source/main.cpp
@@ -0,0 +1,26 @@
+#include "Include/Port.h"
+
+int main() {
+    if (wsInit()) {
+        SOCKET sock;
+        if (CreateSocket(sock, SOCK_STREAM, Port)) {
+            std::cout << "Socket created and specified port closed." << std::endl;
+        } else {
+            std::cerr << "Error creating socket or binding." << std::endl;
+        }
+        CloseSocket(sock);
+
+        // Run a program using the system function
+        const char *programToRun = "your_program.exe"; // Change this to the program you want to run
+        int result = system(programToRun);
+        if (result == 0) {
+            std::cout << "Program executed successfully." << std::endl;
+        } else {
+            std::cerr << "Error running program." << std::endl;
+        }
+    } else {
+        std::cerr << "WSA initialization failed." << std::endl;
+    }
+
+    return 0;
+}
